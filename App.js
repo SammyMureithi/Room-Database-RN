72e13465e9896e2e22a1f5142da4649910ebac3a
Sammy Mureithi <sammymureithi20@gmail.com>
2022-11-01 12:48:32 +0300

    Room Database setup

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Room Database</Text>
+    </View>
+  );
+}
+
+export default App;
